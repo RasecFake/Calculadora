99e6efd2dc0930f443dc2b044fcde86bd1826601
estudiante <estudiante@oretania-PC>
2025-10-17 09:59:28 +0200

    Versión 3: agregar multiplicación

--- Calculadora/src/César/Calculadora.java
+++ Calculadora/src/César/Calculadora.java
@@ -8,4 +8,8 @@ public class Calculadora {
 		 public double restar(double a, double b) {
 			 return a - b;
 		 }
+		 public double multiplicar(double a, double b) {
+			 return a * b;
+		}
+		 
 	}
