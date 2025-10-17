cbfbf829b165452657faeb4e3f514e16aca21cbf
estudiante <estudiante@oretania-PC>
2025-10-17 09:05:26 +0200

    Version 1.1

---
+++ bienvenido/src/bienvenido/Adios.java
@@ -0,0 +1,8 @@
+package bienvenido;
+
+public class Adios {
+	public static void main(String[] Args) {
+			
+			System.out.println("Adios mundo!!!!!");
+	}
+}
---
+++ bienvenido/src/bienvenido/Hola.java
@@ -0,0 +1,8 @@
+package bienvenido;
+
+public class Hola {
+	public static void main(String[] Args) {
+		
+		System.out.println("Hola mundo!!!!!");
+	}
+}
