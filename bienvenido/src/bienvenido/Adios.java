a2bb22be22453443872c36a8f5a4a004aa5f2dc4
estudiante <estudiante@oretania-PC>
2025-10-17 09:12:42 +0200

    version 2.0

--- bienvenido/src/bienvenido/Adios.java
+++ bienvenido/src/bienvenido/Adios.java
@@ -1,8 +1,9 @@
 package bienvenido;
 
-public class adios {
+public class Adios {
 	public static void main(String[] Args) {
 			
+			System.out.println("Adios mundo!!!!!");
 			System.out.println("Adios mundo!!!!!");
 	}
 }
